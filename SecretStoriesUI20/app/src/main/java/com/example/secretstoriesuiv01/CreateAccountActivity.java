bb8a06f2dfeb55da12526a5564900c081aed87a1
Fredrik Johnson <fredrik1010@gmail.com>
2019-05-26 15:48:47 +0200

    Version 20
    Version 20 av systemet

---
+++ SecretStoriesUI20/app/src/main/java/com/example/secretstoriesuiv01/CreateAccountActivity.java
@@ -0,0 +1,74 @@
+package com.example.secretstoriesuiv01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private EditText chatPassword;
+    private EditText password2;
+    private EditText chatPassword2;
+    public static Client client;
+    private ConnectDB database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        username = findViewById(R.id.input_username);
+        password = findViewById(R.id.input_password);
+        chatPassword = findViewById(R.id.chat_password);
+        password2 = findViewById(R.id.input_password2);
+        chatPassword2 = findViewById(R.id.chat_password2);
+
+
+         // "192.168.1.104"
+    }
+
+    public void toLogin(View view){
+        finish();
+    }
+
+    public void createAccount(View view){
+        //TODO check if first and surname is entered
+        Toast.makeText(this, username.getText().toString() + " " + password.getText().toString(), Toast.LENGTH_SHORT).show();
+
+        String userString = username.getText().toString();
+        String passwordString = password.getText().toString();
+        String chatPasswordString = chatPassword.getText().toString();
+        String passwordString2 = password2.getText().toString();
+        String chatPasswordString2 = chatPassword2.getText().toString();
+
+        if(!(passwordString.equals(passwordString2) || chatPasswordString.equals(chatPasswordString2))){
+            Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_LONG).show();
+        }
+        else if(userString.length() <= 2){
+            Toast.makeText(this, "Username cannot be under 2 characters.", Toast.LENGTH_LONG).show();
+        }
+        else if(passwordString.length() <= 4){
+            Toast.makeText(this, "Password cannot be under 5 characters.", Toast.LENGTH_LONG).show();
+        }
+        else if(chatPasswordString.length() <= 4){
+            Toast.makeText(this, "Password for chat-lock cannot be under 5 characters", Toast.LENGTH_LONG).show();
+        }
+        else{
+            User user = new User(userString, passwordString, chatPasswordString);
+            client = new Client();
+            client.connect(this);
+            client.createUser(user);
+        }
+
+
+    }
+
+    public void onFinishCallback()
+    {
+        this.finish();
+    }
+}
---
+++ SecretStoriesUI20/app/src/main/java/com/example/secretstoriesuiv01/Message.java
@@ -0,0 +1,80 @@
+package com.example.secretstoriesuiv01;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class Message implements Serializable {
+	private int key;
+	private String text;
+	private byte[] encryptedMessage;
+	private String sender;
+	private ArrayList<String> recipients;
+	private Date dateSent;
+
+	//If the user sends a message-object without a picture
+
+	public Message(String text, String sender, ArrayList<String> recipients, int key) {
+		this.text = text;
+		this.sender = sender;
+		this.recipients = recipients;
+		this.key = key;
+	}
+
+	//If the user sends a message-object with a picture
+	//public Message2(int id, String text, String sender, Object[] recipients) {
+	//	this(id, text, sender, recipients);
+	//}
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public ArrayList<String> getRecipients() {
+		return recipients;
+	}
+
+	public void setRecipients(ArrayList<String> recipients) {
+		this.recipients = recipients;
+	}
+
+//	public ImageIcon getImage() {
+//		return image;
+//	}
+
+	//	public void setImage(ImageIcon image) {
+//		this.image = image;
+//	}
+	public void setDateSent(Date date) {
+		this.dateSent = dateSent;
+	}
+	public Date getDateSent(Date date) {
+		return dateSent;
+	}
+	public int getKey() {
+		return key;
+	}
+	public void setKey(int key) {
+		this.key = key;
+	}
+	public void setEncryptedMessage(byte[] message){
+		this.encryptedMessage = message;
+	}
+	public byte[] getEncryptedMessage(){
+		return encryptedMessage;
+	}
+
+
+}
